f00830db71868b357adaa7cc035c366a902379e0
joh-lu <77614653+joh-lu@users.noreply.github.com>
2022-02-23 13:10:30 +0100

    Update api.py
    Added some request-methods

--- api.py
+++ api.py
@@ -1,82 +1,120 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify, abort
 import uuid
 
 app = Flask(__name__)
 
+# create unique id for lists, users, entries
+user_id_bob = uuid.uuid4()
+user_id_alice = uuid.uuid4()
+user_id_eve = uuid.uuid4()
+todo_list_1_id = '1318d3d1-d979-47e1-a225-dab1751dbe75'
+todo_list_2_id = '3062dc25-6b80-4315-bb1d-a7c86b014c65'
+todo_list_3_id = '44b02e00-03bc-451d-8d01-0c67ea866fee'
+todo_1_id = uuid.uuid4()
+todo_2_id = uuid.uuid4()
+todo_3_id = uuid.uuid4()
+todo_4_id = uuid.uuid4()
 
-class List:
-    def __init__(self, id, name, todos):
-        self.id = id
-        self.name = name
-        self.todos = todos
+# define internal data structures with example data
+user_list = [
+    {'id': user_id_bob, 'name': 'Bob'},
+    {'id': user_id_alice, 'name': 'Alice'},
+    {'id': user_id_eve, 'name': 'Eve'},
+]
+todo_lists = [
+    {'id': todo_list_1_id, 'name': 'Einkaufsliste'},
+    {'id': todo_list_2_id, 'name': 'Arbeit'},
+    {'id': todo_list_3_id, 'name': 'Privat'},
+]
+todos = [
+    {'id': todo_1_id, 'name': 'Milch', 'description': '', 'list': todo_list_1_id, 'user': user_id_bob},
+    {'id': todo_2_id, 'name': 'Arbeitsblätter ausdrucken', 'description': '', 'list': todo_list_2_id, 'user': user_id_alice},
+    {'id': todo_3_id, 'name': 'Kinokarten kaufen', 'description': '', 'list': todo_list_3_id, 'user': user_id_eve},
+    {'id': todo_3_id, 'name': 'Eier', 'description': '', 'list': todo_list_1_id, 'user': user_id_eve},
+]
 
+# add some headers to allow cross origin access to the API on this server, necessary for using preview in Swagger Editor!
+@app.after_request
+def apply_cors_header(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET,POST,DELETE'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    return response
 
-class ToDo:
-    def __init__(self, id, name, description, user_id):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.user_id = user_id
+# define endpoint for getting and deleting existing todo lists
+@app.route('/todo-list/<list_id>', methods=['GET', 'DELETE'])
+def handle_list(list_id):
+    # find todo list depending on given list id
+    list_item = None
+    for l in todo_lists:
+        if l['id'] == list_id:
+            list_item = l
+            break
+    # if the given list id is invalid, return status code 404
+    if not list_item:
+        return 'BLABLA'
+        #abort(404)
+    if request.method == 'GET':
+        # find all todo entries for the todo list with the given id
+        print('Returning todo list...')
+        #return '', 200 
+        return jsonify([i for i in todos if i['list'] == list_id])
+    elif request.method == 'DELETE':
+        # delete list with given id
+        print('Deleting todo list...')
+        todo_lists.remove(list_item)
+        return '', 200
 
-
-class User:
-    def __init__(self, id, name):
-        self.id = id
-        self.name = name
-
-
-#zeigt alle Todo-Listen
+ 
+#schows all Todo-Lists
 @app.route("/todo-list", methods=['GET'])
 def getlist():
-    return "GETLIST"
+    return jsonify(todo_lists)
 
+
+"""
 #fügt neue Todo-Liste hinzu
 @app.route("/todo-list", methods=['POST'])
 def postlist():
     return "POSTLIST"
+ """
 
-#zeigt alle Einträge einer Todo-Liste
-@app.route("/todo-list/<string:list_id>", methods=['GET'])
-def getlistid():
-    return "GETLISTID"
-
-#löscht eine Todo-Liste
-@app.route("/todo-list/<string:list_id>", methods=['DELETE'])
-def deletelistid():
-    return "DELETELISTID"
-
+"""
 #fügt einer Liste einen neuen Eintrag hinzu
-@app.route("/todo-list/<string:list_id>/entry", methods=['POST'])
+@app.route("/todo-list/<list_id>/entry", methods=['POST'])
 def postentry():
     return "POSTENTRY"
 
 #aktualisiert einen Eintrag
-@app.route("/todo-list/<string:list_id>/entry/<string:entry_id>", methods=['PUT'])
+@app.route("/todo-list/<list_id>/entry/<entry_id>", methods=['PUT'])
 def putentryid():
     return "PUTENTRYID"
 
 #löscht einen Eintrag
-@app.route("/todo-list/<string:list_id>/entry/<string:entry_id>", methods=['DELETE'])
+@app.route("/todo-list/<list_id>/entry/<entry_id>", methods=['DELETE'])
 def deleteentryid():
     return "DELETEENTRYID"
 
-
-#zeigt alle User
+"""
+#shows all user
 @app.route("/user", methods=['GET'])
 def getuser():
-    return "GETUSER"
+    return jsonify(user_list)
 
+
+"""
 #fügt einen neuen User hinzu
 @app.route("/user", methods=['POST'])
 def postuser():
     return "POSTUSER"
 
 #löscht einen User
-@app.route("/user/<string:user_id>", methods=['DELETE'])
+@app.route("/user/<user_id>", methods=['DELETE'])
 def deleteuser():
     return "DELETEUSER"
-
+ """
 
 #Server starten
 if __name__ == '__main__':
-    app.run(host='')
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
